3a27497baaf737c81ac6b0c03a5b87726384829b
wein66 <40013461+wein66@users.noreply.github.com>
2023-02-27 17:59:33 +0900

    날씨앱 2.27작업

--- js/custom.js
+++ js/custom.js
@@ -6,4 +6,34 @@ $(function(){
       centerMode: true,
       focusOnSelect: true
    });
+
+   $('#searchbtn').on('click', function(){
+       if($('.searchinput').hasClass('act')){
+          $('.searchinput').removeClass('act'); 
+       }else{
+          $('.searchinput').addClass('act');
+          $('.searchinput input').focus();
+       }
+   });
+
+  $('#search-city').on('keypress', function(e){
+      if(e.which == 13 && !e.shiftKey) {
+         const key = $(this).val();
+         console.log(key);
+         $(this).val('');
+         $('.searchinput').removeClass('act'); 
+      }
+  });
 }); //jquery
+
+function getWeatherLocation(){
+    navigator.geolocation.getCurrentPosition(
+        onWeatherSuccess, onWeatherError, { enableHighAccuracy: true }
+    );
+}
+
+const onWeatherSuccess = function (position) {
+    const lat = position.coords.latitude;
+    const lon = position.coords.longitude;
+    console.log(lat, lon);
+}
