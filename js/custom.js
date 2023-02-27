61004a64c527119a42388c8d37a9613024ecfaee
wein66 <40013461+wein66@users.noreply.github.com>
2023-02-27 16:57:19 +0900

    날씨앱 디자인 작업완료

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,9 @@
 $(function(){
-
+   $('.threetemp-body').slick({
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      dots: false,
+      centerMode: true,
+      focusOnSelect: true
+   });
 }); //jquery
