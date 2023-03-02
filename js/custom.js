6ce24b5f6091c19b16a783c4e46c04ebd16adb7f
wein66 <40013461+wein66@users.noreply.github.com>
2023-03-02 17:45:12 +0900

    api 데이터 받기

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,7 @@
 $(function(){
+   let key = '';
+   let myLat = 0, myLon = 0;
+
    $('.threetemp-body').slick({
       slidesToShow: 3,
       slidesToScroll: 1,
@@ -19,21 +22,59 @@ $(function(){
   $('#search-city').on('keypress', function(e){
       if(e.which == 13 && !e.shiftKey) {
          const key = $(this).val();
-         console.log(key);
+         //console.log(key);
          $(this).val('');
          $('.searchinput').removeClass('act'); 
+         getWeather('', '', key);
       }
   });
+
+  if(key == ''){
+     if(navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position){
+            myLat = position.coords.latitude;
+            myLon = position.coords.longitude;
+            getWeather(myLat, myLon, '');
+        });
+     }
+  }
+
 }); //jquery
 
-function getWeatherLocation(){
-    navigator.geolocation.getCurrentPosition(
-        onWeatherSuccess, onWeatherError, { enableHighAccuracy: true }
-    );
+
+
+function getWeather(lat, lon, city){
+    const mykey = '61817fe9871c5ce196a7b67a92ce3a6b';
+    const url = '//api.openweathermap.org/data/2.5/forecast';
+    let wdata;
+    if(city){
+       wdata = {
+        q : city,
+        appid: mykey,
+        units: 'metric',
+        lang: 'kr'
+       }
+    }else{
+       wdata = {
+        lat : lat,
+        lon : lon,
+        appid: mykey,
+        units: 'metric',
+        lang: 'kr'
+       } 
+    }
+    //console.log(wdata);
+    $.ajax({
+        dataType: 'json',
+        url: url,
+        data: wdata,
+        type: 'GET',
+        success: function(data) {
+            console.log(data);
+        },
+        error: function(xhr, status, error) {
+            console.log(error);
+        }
+    })
 }
 
-const onWeatherSuccess = function (position) {
-    const lat = position.coords.latitude;
-    const lon = position.coords.longitude;
-    console.log(lat, lon);
-}
